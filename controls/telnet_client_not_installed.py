c1d7a8b0a9a3d0e011e63fba8f18866e5aa536de
rajin4463 <rajinsandira@outlook.com>
2023-07-06 13:18:47 +0530

    changes

--- controls/telnet_client_not_installed.py
+++ controls/telnet_client_not_installed.py
@@ -4,4 +4,9 @@ def telnet():
     command = "dpkg-query -W -f='${binary:Package}\t${Status}\t${db:Status-Status}\n' telnet"
     process=subprocess.run(command, shell=True, stdout=subprocess.PIPE)
     output = process.stdout
-    print(output)
+    compare = "telnet	install ok installed	installed"
+    
+    if compare in output:
+        print("Desired string found in the command output!")
+    else:
+        print("Desired string not found in the command output.")
