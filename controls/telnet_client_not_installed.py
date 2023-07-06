d3b4047bbe9fb2dbd0dc5dbec36e7a7ab9b894e1
rajin4463 <rajinsandira@outlook.com>
2023-07-06 13:05:11 +0530

    changes

--- controls/telnet_client_not_installed.py
+++ controls/telnet_client_not_installed.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def telnet():
-    command = ["dpkg-query", "-W", "-f='${binary:Package}\t${Status}\t${db:Status-Status}\n'", "telnet"]
+    command = ["dpkg-query", "-W", "-f", "=", "'","$","{binary:Package}\t${Status}\t${db:Status-Status}\n", "'", "telnet"]
     process=subprocess.run(command, shell=True, stdout=subprocess.PIPE)
     output = process.stdout
     print(output)
