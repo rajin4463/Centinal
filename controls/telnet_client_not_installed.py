2954d9f975eb27153af1500e5d4c19e1ec9f97ef
rajin4463 <rajinsandira@outlook.com>
2023-07-06 09:13:00 +0530

    Updated telnet check

--- controls/telnet_client_not_installed.py
+++ controls/telnet_client_not_installed.py
@@ -2,5 +2,5 @@ import subprocess
 
 def telnet():
     command = "dpkg-query -W -f='${binary:Package}\t${Status}\t${db:Status-Status}\n' telnet"
-    # process=subprocess.check_output(command, shell=True, stdout=subprocess.PIPE)
-    print(command)
+    process=subprocess.check_output(command, shell=True, stdout=subprocess.PIPE)
+    print(process)
