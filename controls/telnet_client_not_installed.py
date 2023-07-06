1fcb5d36cae11700f46d9e49c17060f2ec308bf9
rajin4463 <rajinsandira@outlook.com>
2023-07-06 17:25:14 +0530

    added amination changes

--- controls/telnet_client_not_installed.py
+++ controls/telnet_client_not_installed.py
@@ -1,5 +1,8 @@
 import subprocess
+import time
 from colorama import Fore, Style
+
+x = 1
 
 def telnet():
     compare = "telnet	install ok installed	installed"
@@ -8,11 +11,13 @@ def telnet():
     output = process.stdout.decode('utf-8')
 
     if compare in output:
-        print(Fore.RED + "\n\033[1m[-] telnet client was found.")
+        print(Fore.RED + "\n\033[1m[+] telnet client was found.")
         print("\n\033[1m[-] Purging client" + Style.RESET_ALL)
-        purgeCommand = "apt purge telnet"
+        purgeCommand = ["apt-get", "purge", "-y", "telnet"]
         process = subprocess.run(purgeCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        animation()
         if process.returncode == 0:
+            x = 0
             print(Fore.GREEN + "\n\033[1m[+] Successfully purged Telnet Client." + Style.RESET_ALL)
         else:
             print(Fore.RED + "\n\033[1m[-]telnet client purge FAILED: error something went wrong\n" + Style.RESET_ALL)
@@ -20,3 +25,11 @@ def telnet():
     else:
         print(Fore.RED + "\n\033[1m[-]telnet client purge FAILED: error telnet client dosen't seem to be installed.")
         print(output)
+
+def animation():
+    animation = "|/-\\"
+    idx = 0
+    while x==1:
+        print(Fore.GREEN + '...' + animation[idx % len(animation)], end="\r" + Style.RESET_ALL)
+        idx += 1
+        time.sleep(0.1)
