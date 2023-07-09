c1ec9c89fe0247bf9dc1999feee336ae6b606999
rajin4463 <rajinsandira@outlook.com>
2023-07-09 10:19:01 +0530

    errors fix test

--- controls/local_login_warning_banner.py
+++ controls/local_login_warning_banner.py
@@ -10,12 +10,12 @@ def localLogingWarning():
         with open(fileName, 'r') as file:
             lines = file.readlines()
         file.close()
+        print(lines)
         if any(desired_string in line for line in lines):
             with open(fileName, 'a') as file:
                 file.write(banner_text)
                 print(Fore.GREEN + "\n\033[1m[+] Local Login Warning Banner change SUCCESS: banner changed!" + Style.RESET_ALL)
             file.close()
-            print(lines)
         else:
             print(Fore.RED + "\n\033[1m[-] Local Login Warning Banner change FAILED: error file seems to be changed" + Style.RESET_ALL)
     else:
