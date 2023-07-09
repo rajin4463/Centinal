2c7b5ba18998f2564cb50c927aaae853d44ee33d
rajin4463 <rajinsandira@outlook.com>
2023-07-09 12:12:14 +0530

    fix for local login banner wanning not showing up test 2

--- controls/local_login_warning_banner.py
+++ controls/local_login_warning_banner.py
@@ -1,8 +1,10 @@
 import os
 from colorama import Fore, Style
 fileName = '/etc/issue'
+bashFile = '~/.bashrc'
 banner_text = """WARNING: Unauthorized access to this system is prohibited.
 By accessing this system, you agree that your actions may be monitored and recorded."""
+bashFileCommand = "cat /etc/issue"
 desired_string = "Ubuntu 22.04"
 def localLogingWarning():
     if os.path.exists(fileName):
@@ -10,11 +12,20 @@ def localLogingWarning():
             lines = file.readlines()
         file.close()
         if any(desired_string in line for line in lines):
-            with open(fileName, 'w') as file:
-                file.write(banner_text)
-                print(Fore.GREEN + "\n\033[1m[+] Local Login Warning Banner change SUCCESS: banner changed!" + Style.RESET_ALL)
-            file.close()
+            modifyFiles()
         else:
             print(Fore.RED + "\n\033[1m[-] Local Login Warning Banner change FAILED: error file seems to be changed" + Style.RESET_ALL)
     else:
         print(Fore.RED + '\n\033[1m[-] Local Login Warning Banner change FAILED: error file not found' + Style.RESET_ALL)
+
+def modifyFiles():
+    try:
+        with open(fileName, 'w') as file:
+            file.write(banner_text)
+        file.close()
+        with open(bashFile, 'a') as file:
+            file.write(bashFileCommand)
+        file.close()
+        print(Fore.GREEN + '\n\033[1m[+] Local Login Warning Banner change SUCCESS: banner changed!' + Style.RESET_ALL)
+    except:
+        print(Fore.RED + '\n\033[1m[-] Local Login Warning Banner change FAILED: error something went wrong' + Style.RESET_ALL)
