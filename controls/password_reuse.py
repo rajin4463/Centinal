9ea55b592a424282c5c06c3e30dff78bfbe190f5
rajin4463 <rajinsandira@outlook.com>
2023-09-07 17:25:00 +0530

    password reuse limit set

---
+++ controls/password_reuse.py
@@ -0,0 +1,18 @@
+import os
+import misc.logger
+from colorama import Fore, Style
+logger = misc.logger.setup_logger()
+
+file_name = '/etc/pam.d/common-password'
+remember_option = '5'
+
+def edit_pam_file():
+  with open(file_name, 'r') as f:
+    lines = f.readlines()
+
+  for i, line in enumerate(lines):
+    if line.startswith('password required pam_pwcheck.so'):
+      lines[i] = 'password required pam_pwcheck.so nullok remember=' + remember_option
+
+  with open(file_name, 'w') as f:
+    f.writelines(lines)
